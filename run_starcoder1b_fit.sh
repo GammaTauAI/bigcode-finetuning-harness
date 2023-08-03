e6b44c92efb20c9c81a72a5b28e65494334256ae
cassanof <federico.cassano@federico.codes>
2023-08-03 10:31:00 -0700

    a

--- run_starcoder1b_fit.sh
+++ run_starcoder1b_fit.sh
@@ -12,6 +12,7 @@ python3 -m torch.distributed.launch \
         --no_custom_tokenizer \
         --dataset_name="nuprl/ts-training" \
         --dataset_revision="v1.1p1" \
+        --total_tokens=7203565775 \
         --fim_rate 1 \
         --fim_spm_rate 0.5 \
         --output_dir="./model_starcoder_1b_fit_8k" \
